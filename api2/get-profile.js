f19954050152b9bb1ecff83e63836c352bffa2d5
Irra11 <boraffcremix@gmail.com>
2026-05-02 15:57:27 +0700

    Update get-profile.js

--- api2/get-profile.js
+++ api2/get-profile.js
@@ -1,6 +1,12 @@
+import crypto from 'crypto';
+
 export default function handler(req, res) {
+  // Get the base domain (e.g., https://example.com/)
   const rootUrl = `https://${req.headers.host}/`;
-  const enrollUrl = `${rootUrl}api2/enroll`;
+  
+  // Point the enrollment to the new folder path
+  // Usually, the POST request should go to the same folder or a sub-route
+  const enrollUrl = `${rootUrl}api2/enroll`; 
 
   const profileXml = `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -26,7 +32,7 @@ export default function handler(req, res) {
     <key>PayloadUUID</key>
     <string>${crypto.randomUUID()}</string>
     <key>PayloadIdentifier</key>
-    <string>com.irra.udid</string>
+    <string>com.irra.udid.api2</string>
     <key>PayloadType</key>
     <string>Profile Service</string>
 </dict>
