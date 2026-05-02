1f5990fd2d764d594839ec6337e02969a6cc22ec
Irra11 <boraffcremix@gmail.com>
2026-05-02 15:48:30 +0700

    Update get-profile.js

--- app/get-profile.js
+++ app/get-profile.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   const rootUrl = `https://${req.headers.host}/`;
-  const enrollUrl = `${rootUrl}app/enroll`;
+  const enrollUrl = `${rootUrl}api/enroll`;
 
   const profileXml = `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
